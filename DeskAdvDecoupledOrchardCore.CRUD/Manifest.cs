61a61feb8aa2f0995bbd45524b90ef876592e1d6
TihonchukArtyom89 <53569237+TihonchukArtyom89@users.noreply.github.com>
2021-07-16 12:34:29 +0300

    Initial commit

---
+++ DeskAdvDecoupledOrchardCore.CRUD/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "DeskAdvDecoupledOrchardCore.CRUD",
+    Author = "Artyom Tihonchuk",
+    Website = "",
+    Version = "0.0.1",
+    Description = "CRUD operations with Ad",
+    Category = "Content Management"
+)]
---
+++ README.md
@@ -0,0 +1,2 @@
+# DeskAdvDecoupledOrchardCore
+ Training project. Desk of advertisments, CRUD adv, different user permission. Created on Orchard Core.
